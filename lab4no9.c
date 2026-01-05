50d703b175c46f3c0a20749efe7489e414c0a42a
6804062612323-Thanapat <s6804062612323@email.kmutnb.ac.th>
2026-01-06 02:43:30 +0700

    Add files via upload

---
+++ lab4no9.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+int score;
+int n,i;
+
+while (1) {
+    
+    scanf("%d",&score);
+
+    if (score == -1) {
+        break;
+    }
+
+    if (score <= 100 && score >= 0 )	{
+        if (score >= 68) {
+	        if (score >= 85)
+		        printf("%d(A)\n",score);
+	        else if (score >= 75)
+		        printf("%d(b)\n",score);
+	        else 
+		        printf("%d(C)\nneed  %d score to B\n",score,75-score); //bonus 1
+		
+    } 
+    else {
+	    if (score >= 55)
+		    printf("%d(D)\n",score);
+	    else 
+		    printf("%d(E)\n",score);
+    }
+}
+else { //bonus 2
+	printf("error score");
+}
+}
+
+	
+}
+
+
+
+
+
